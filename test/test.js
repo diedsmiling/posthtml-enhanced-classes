bb8806d4a6a55dbf21e96f8b1c1be0b3860378bf
lazarev <diedsmiling@gmail.com>
2016-07-05 23:56:41 +0300

    First commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const posthtml = require('posthtml');
+const expect = require('chai').expect;
+
+describe('Test test', function() {
+  it('test test', function(done) {
+    expect(1).to.equal(1)
+  });
+
+});
