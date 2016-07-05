06ebe68825d073c9dfd53b5b936d4b820c7a27d3
lazarev <diedsmiling@gmail.com>
2016-07-06 00:11:21 +0300

    eslint initialized

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const posthtml = require('posthtml');
-const expect = require('chai').expect;
+const posthtml = require('posthtml')
+const expect = require('chai').expect
 
 describe('Test test', function() {
   it('test test', function(done) {
     expect(1).to.equal(1)
-  });
+  })
 
-});
+})
